1bb6e21ddf83107fcb219ca915b924cc4158e3cc
Mamdouh66 <2210002016@iau.edu.sa>
2023-03-28 19:40:04 +0300

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import styles from "./style";
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
+      {/*This is the navbar section*/}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      {/*This is the hero section*/}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats business billing cardDeal Testimonials Clients CTA Footer
+        </div>
       </div>
     </div>
   );
