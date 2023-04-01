c8ec8f75a59b86168e94a8280ff546b610b958ba
Mamdouh66 <2210002016@iau.edu.sa>
2023-04-01 04:47:47 +0300

    Started FastHacks

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 } from "./components";
 const App = () => {
   return (
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-[#F0EAD6] w-full overflow-hidden">
       {/*This is the navbar section*/}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
@@ -23,13 +23,13 @@ const App = () => {
       </div>
 
       {/*This is the hero section*/}
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-[#F0EAD6] ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`bg-[#F0EAD6] ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />{" "}
           <Clients /> <CTA /> <Footer />
